12835c78861419048e6ade1aecbc426db570fe88
fanhb <fanhb@ziroom.com>
2017-12-08 14:17:00 +0800

    格式化

--- git.php
+++ git.php
@@ -10,9 +10,9 @@ set_time_limit(0);
 date_default_timezone_set('Asia/Shanghai');
 
 $config = array(
-    'token_field'   =>  'X-Hub-Signature',
-    'access_token'  =>  'xxxxxxxxx',
-    'bash_path'     =>  '/data/html/git-update/sh/hexo-pull.sh',
+    'token_field' => 'X-Hub-Signature',
+    'access_token' => 'xxxxxxxxx',
+    'bash_path' => '/data/html/git-update/sh/hexo-pull.sh',
 );
 
 $webhook = new Webhook($config);
